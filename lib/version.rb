a7073f387dd9e17582fb1c2ef62ed5b7ab561360
jltml <8261330+jltml@users.noreply.github.com>
2023-11-13 10:38:06 -0500

    Bump to 0.0.4

--- lib/version.rb
+++ lib/version.rb
@@ -1,5 +1,5 @@
 module ArtShop
   # Current Version
   # @return [String]
-  VERSION = "0.0.3".freeze unless const_defined?(:VERSION)
+  VERSION = "0.0.4".freeze unless const_defined?(:VERSION)
 end
